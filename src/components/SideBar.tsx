c9198d3da84eee9412849a28323b3a49da7ead72
Calado <wishsolidor@gmail.com>
2022-06-26 06:14:22 -0300

    refactor: improve maintainability

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -18,22 +18,29 @@ const GET_LESSONS_QUERY = gql`
 const SideBar: React.FC = () => {
   const { data, loading } = useQuery<LessonResponse>(GET_LESSONS_QUERY);
 
+  const renderLessons = () => {
+    /*TODO: IMPLEMENT SKELETON */
+    if (loading) return <h1>Carregando...</h1>;
+
+    /*TODO: SHOW EMPTY DATA VIEW */
+    if (data!.lessons.length <= 0 || data === undefined )
+      return <h1>Nada por aqui</h1>;
+
+    return data.lessons.map(({ id, availableAt, ...rest }) => (
+      <LessonCard
+        key={id}
+        lesson={{ availableAt: new Date(availableAt), ...rest }}
+      />
+    ));
+  };
+
   return (
     <aside className="w-[348px] p-6 bg-gray-700 border-l border-gray-600">
       <span className="block font-bold text-2xl pb-6 mb-6 border-b border-gray-500">
         Cronograma de aulas
       </span>
 
-      {/*TODO: IMPLEMENT SKELETON */}
-
-      <div className="flex flex-col gap-8">
-        {data?.lessons.map(({ id, availableAt, ...rest }) => (
-          <LessonCard
-            key={id}
-            lesson={{ availableAt: new Date(availableAt), ...rest }}
-          />
-        ))}
-      </div>
+      <div className="flex flex-col gap-8">{renderLessons()}</div>
     </aside>
   );
 };
